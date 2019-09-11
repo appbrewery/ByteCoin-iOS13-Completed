2e7fb434efd68cb7bc5ed9270194421505d770fe
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-11 16:09:36 +0100

    Step 3 Solution

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -15,6 +15,29 @@ struct CoinManager {
     
     func getCoinPrice(for currency: String) {
         
+        //Use String concatenation to add the selected currency at the end of the baseURL.
+        let urlString = baseURL + currency
+        
+        //Use optional binding to unwrap the URL that's created from the urlString
+        if let url = URL(string: urlString) {
+            
+            //Create a new URLSession object with default configuration.
+            let session = URLSession(configuration: .default)
+            
+            //Create a new data task for the URLSession
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                print(data!)
+                
+            }
+            //Start task to fetch data from bitcoin average's servers.
+            task.resume()
+        }
     }
+    
     
 }
