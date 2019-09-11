721fcd92a65ae45604229ea29f5edb36efbb0c29
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-11 16:55:23 +0100

    Remove comments

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,34 +8,25 @@
 
 import UIKit
 
-//Adopt the Coin Manager Protocol
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
     
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-    //Need to change this to a var to be able to modify its properties.
     var coinManager = CoinManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Easily Missed: Must set the coinManager's delegate as this current class so that we can recieve
-        //the notifications when the delegate methods are called.
         coinManager.delegate = self
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
         
     }
     
-    //Provide the implementation for the delegate methods.
-    
-    //When the coinManager gets the price it will call this method and pass over the price and currency.
     func didUpdatePrice(price: String, currency: String) {
         
-        //Remember that we need to get hold of the main thread to update the UI, otherwise our app will crash if we
-        //try to do this from a background thread (URLSession works in the background).
         DispatchQueue.main.async {
             self.bitcoinLabel.text = price
             self.currencyLabel.text = currency
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,22 +8,13 @@
 
 import Foundation
 
-//By convention, Swift protocols are usually written in the file that has the class/struct which will call the
-//delegate methods, i.e. the CoinManager.
 protocol CoinManagerDelegate {
-    
-    //Create the method stubs wihtout implementation in the protocol.
-    //It's usually a good idea to also pass along a reference to the current class.
-    //e.g. func didUpdatePrice(_ coinManager: CoinManager, price: String, currency: String)
-    //Check the Clima module for more info on this.
     func didUpdatePrice(price: String, currency: String)
     func didFailWithError(error: Error)
 }
 
 struct CoinManager {
     
-    //Create an optional delegate that will have to implement the delegate methods.
-    //Which we can notify when we have updated the price.
     var delegate: CoinManagerDelegate?
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
@@ -43,14 +34,8 @@ struct CoinManager {
                 }
                 
                 if let safeData = data {
-                    
                     if let bitcoinPrice = self.parseJSON(safeData) {
-                        
-                        //Optional: round the price down to 2 decimal places.
                         let priceString = String(format: "%.2f", bitcoinPrice)
-                        
-                        //Call the delegate method in the delegate (ViewController) and
-                        //pass along the necessary data.
                         self.delegate?.didUpdatePrice(price: priceString, currency: currency)
                     }
                 }
