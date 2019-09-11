7273202bf10a20b2c774637f84cb2c83e2387554
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-11 15:18:22 +0100

    Update CoinManager

---
+++ ByteCoin/Model/coinManager.swift
@@ -0,0 +1,18 @@
+//
+//  coinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    
+    
+}
